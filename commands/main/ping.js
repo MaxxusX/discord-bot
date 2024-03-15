1071c4119abd8083290a1fd0db5073b34fb98ac2
MaxxusX <CarterWinden@gmail.com>
2024-03-15 00:32:05 -0500

    fix negative ping

--- commands/main/ping.js
+++ commands/main/ping.js
@@ -6,10 +6,7 @@ module.exports = {
 		.setName('ping')
 		.setDescription('pings the bot'),
 	async execute(interaction) {
-		const delay = Date.now() - interaction.createdAt
 		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
-		const delay2 = (Date.now() - interaction.createdAt) - delay
-		const delay3 = (Date.now() - interaction.createdAt)
-		interaction.editReply(`Roundtrip Latency: ${Date.now() - sent.createdTimestamp}ms\nCreation Delay: ${delay}ms\nMessage Send Delay: ${delay2}ms\nOverall Delay: ${delay3}ms`);
+		await interaction.editReply(`Websocket Heartbeat: ${interaction.client.ws.ping}ms\nRountrip Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
 	},
 };
