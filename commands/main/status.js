0a361568a898d63ed6b468eb6c8c40b9f19501f3
MaxxusX <CarterWinden@gmail.com>
2024-03-15 01:01:02 -0500

    added status command

---
+++ commands/main/status.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	cooldown: 1,
+	data: new SlashCommandBuilder()
+		.setName('status')
+		.setDescription('sends info about the bot'),
+	async execute(interaction) {
+		const milliseconds = interaction.client.uptime
+		const hours = Math.floor((milliseconds / 1000 / 60 / 60) % 24);
+		const minutes = Math.floor((milliseconds / 1000 / 60) % 60);
+		const seconds = Math.floor((milliseconds / 1000) % 60);
+		console.log(interaction.client)
+		await interaction.reply(`Uptime: ${hours}hrs ${minutes}mins and ${seconds}s\nCreated At: ${interaction.client.application.createdAt}`);
+	},
+};
